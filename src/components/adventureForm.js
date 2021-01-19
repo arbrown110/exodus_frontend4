1d94c94a97f61a7f7a24174b3a84f2a94d290ed1
Angel_Brown <arbrown110@gmail.com>
2021-01-18 20:40:37 -0500

    adentureform completed but no items in state.

--- src/components/adventureForm.js
+++ src/components/adventureForm.js
@@ -1,11 +1,26 @@
 import React, { Component  } from 'react';
+import { addAdventure } from '../actions/AdventuresActions'
+import { connect} from 'react-redux'
 
 class AdventureForm extends Component {
 
     state = {
-        name: "",
-        image_url: ""
+        name: '',
+        image_url: ''
     }
+
+    handleChange = e => {
+        const { name, value } = e.target
+        this.setState({
+            [name]: value
+        })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault()
+        this.props.addAdventure(this.state)
+    }
+
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
@@ -24,6 +39,6 @@ class AdventureForm extends Component {
     }
 }
 
-export default AdventureForm;
+export default connect(null, { addAdventure })(AdventureForm);
 
 //storestate == redux store it's global  localstate == component state is isolate to that component only
